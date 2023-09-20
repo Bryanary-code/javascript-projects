465ff976ae6fe61b6a79292fc59f70736ccbb6db
Bryan Wulfert <bwulfert@gmail.com>
2023-09-19 20:07:00 -0500

    studio part 1 code

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -7,10 +7,13 @@ let newStr = str.replace('Lau', '') + 'Lau'
 console.log(str)
 console.log(newStr)
 
+
 //Use a template literal to print the original and modified string in a descriptive phrase.
 console.log(`Through the magic of methods, ${str} was modified to ${newStr}.`)
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 let numOfLetters = input.question("How many letters would you like to move? ")
+
+
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
 if (numOfLetters > str.length) {
 numOfLetters = 3
