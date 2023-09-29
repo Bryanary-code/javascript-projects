2d6e39bc95a040b4d33326ca932268599280253b
Bryan Wulfert <bwulfert@gmail.com>
2023-09-29 14:11:54 -0500

    more functions exercises

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -6,6 +6,16 @@ b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. 
 */
 
+let anon = function(x) {
+    if(typeof x === 'number') {
+      return x * 3
+    } else if (typeof x === 'string') {
+      return 'ARRR!'
+    } else {
+      return x
+    }
+  }
+
 /* Add to your code! Use your fuction and the map method to change an array  as follows:
 a) Triple any the numbers.
 b) Replace any strings with “ARRR!”
@@ -13,3 +23,6 @@ c) Print the new array to confirm your work.
 */
 
 let arr = ['Elocution', 21, 'Clean teeth', 100];
+ 
+let newArr = arr.map(anon)
+ console.log(newArr)
