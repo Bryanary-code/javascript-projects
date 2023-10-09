17e0ceefbaf72aa26599071b87e48bd674bf98d2
Bryan Wulfert <bwulfert@gmail.com>
2023-10-08 21:15:21 -0500

    RPS tests completed and passed

---
+++ unit-testing/exercises/tests/RPS.test.js
@@ -0,0 +1,17 @@
+const whoWon = require('../RPS.js')
+
+describe('whoWon', function (){
+
+    test('should return TIE! if player1 and player2 are equal', function () {
+        expect(whoWon('rock', 'rock')).toBe('TIE!')
+    })
+
+    test('should return Player 2 wins! if player2 wins', function () {
+        expect(whoWon('scissors', 'rock')).toBe('Player 2 wins!')
+    })
+
+    test('should return Player 1 wins! if player1 wins', function () {
+            expect(whoWon('paper', 'rock')).toBe('Player 1 wins!')
+})
+
+});
