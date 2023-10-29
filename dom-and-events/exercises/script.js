d9d8ef2bd18c8285fd6fccee37a6bd4fe74ce0fa
Bryan Wulfert <bwulfert@gmail.com>
2023-10-29 13:34:57 -0500

    completed DOM and events exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,18 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener("click", function(event) {
+        document.getElementById('statusReport').innerHTML = 'Houston! We have liftoff!'
+    })
+    missionAbort.addEventListener('mouseover', function(event) {
+        document.getElementById('abortMission').style.backgroundColor = 'red'
+    })
+    missionAbort.addEventListener('mouseout', function(event) {
+        document.getElementById('abortMission').style.backgroundColor = ''
+    })
+    if(confirm('Are you sure you want to abort the mission?')) {
+        document.getElementById('statusReport').innerHTML = 'Mission aborted! Space shuttle returning home'
+    }
 }
 
 window.addEventListener("load", init);
